81f42cb6426a23d196cadc44f92a6fa476a1c5bb
Funnyweek <kirito.sempai245@gmail.com>
2025-11-11 21:24:36 +0500

    123

---
+++ new_lessons/lesson_2.py
@@ -0,0 +1,97 @@
+
+# a = int(input("ozenca = "))
+# if a >= 90:
+#     print("A")
+# elif a >= 80:
+#     print("B")
+# elif a >= 70:
+#     print("C")
+# elif a >= 60:
+#     print("D")
+# elif a <= 59:
+#     print("F")
+
+# age = int(input("age = "))
+# if age <= 13:
+#     print("Rebenok")
+# elif age > 13 and age <= 17:
+#     print("Podrostok")
+# elif age > 17 and age <= 64:
+#     print("Vzrosluy")
+# else:
+#     print("Pensiomer")
+
+# a = input("слово = ")
+# if len(a)%2 == 0:
+#     print("crtnoe")
+# else:
+#     print("ne chet")
+
+# log = input("Log")
+# pas = int(input("pas"))
+
+# if log == "admin" and pas == 12345:
+#     print("common")
+# else: 
+#     print("not common")
+
+# time = int(input("time"))
+# if time >= 0 and time <= 11:
+#     print("morning")
+# elif time > 11 and time <= 17:
+#     print("afternoon")
+# elif time > 17 and time == 23:
+#     print("nidht")
+
+# temp = int(input("temp = "))
+# if temp < 0:
+#     print("holodno")
+# elif temp >= 0 and temp <= 15:
+#     print("holodno")
+# elif temp >= 16 and temp <= 25:
+#     print("ZARA")
+# elif temp >= 26:
+#     print("PIZDEC")
+
+
+
+# speed = int(input("spedd = "))
+# if speed <= 60 :
+#     print("no")
+# elif speed <= 100:
+#     print("predupez")
+# elif speed >= 100:
+#     print("500 TG")
+
+
+
+# age = int(input("age = "))
+# dohod = int(input("Dohod = "))
+
+# if age >= 21 and dohod >= 50000:
+#     print("odobreno")
+# else:
+#     print("no")
+
+
+# zena = int(input("Suma = "))
+# if zena <= 1000:
+#     print("0%")
+# elif zena <= 1001 and zena >=10000:
+#     print("5%")
+# elif zena <= 10001 and zena >= 20000:
+#     print("10%")
+# else:
+#     print("15%")
+
+
+
+# age = int(input("age = "))
+# stage = int(input("stage = "))
+
+# if age >= 25 and stage >= 3:
+#     print("you are cvelocome")
+# elif age < 25:
+#     print("NOOOO")
+# elif stage < 3:
+#     print("No")
