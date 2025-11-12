60c4df3a6082381d316a70629460474f00cccd53
Funnyweek <kirito.sempai245@gmail.com>
2025-11-12 22:02:08 +0500

    12.11.25-2

---
+++ new_lessons/lesson3.py
@@ -0,0 +1,71 @@
+# text = ("готовые пельмешки")
+# print(text.upper())
+# print(text.lower())
+# print(text.capitalize())
+# print(text.title())
+
+# a = input("a = ")
+# b = input("b = ")
+# print(a.isalpha())
+# print(b.isdigit())
+# print("a = ", len(a))
+# print("b = ", len(b))
+
+# text = input("")
+# a = input("word 1")
+# b = input("word 2")
+# print(text.replace(a, b))
+# print(text.count(b))
+
+# a = "Лучшая пельменная в городе"
+
+# b = a.split(" ")
+# print(b)
+# print(len(b))
+# c = "-".join(b)
+# print(c)
+
+# c = input("File = ")
+# if c.lower().startswith("a"):
+#     print("True")
+# else:
+#     print("false")
+# if c.lower().endswith(".txt"):
+#     print("true")
+# else:
+#     print("false")
+
+
+# list = ["artur", "ellisabet", "melliydas", "escanor", "ban"]
+# print(len(list))
+# list.append(["merelin", "elisabet"])
+# print(list)
+# list2 = ["gauther", "diana"]
+# list.extend(list2)
+# print(list)
+
+# list = ['яблоко', 'банан', 'апельсин', 'груша', 'банан']
+# a = input("a = ")
+# n = input("n = ")
+# b = list.index(a)
+# print(b)
+# c = list.count(a)
+# list.remove(n)
+# print (list)
+
+# list = ["1", "2", "6", "9", "16"]
+# print(list)
+# list.sort()
+# print(list)
+# list.reverse()
+# print(list)
+
+list = ["A", "B", "C", "D"]
+print(list)
+a = input()
+b = input()
+c = list.insert(a, f"{b}")
+print(list)
+n = input()
+no = list.pop(n)
+print(list)
