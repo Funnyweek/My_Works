45dc85e1cabeb5b70504226b4be291f89a887c3e
Funnyweek <kirito.sempai245@gmail.com>
2025-11-14 19:50:46 +0500

    13.11.25-1

---
+++ new_lessons/123.py
@@ -0,0 +1,11 @@
+a = int(input("Введите a: "))
+b = int(input("Введите b: "))
+c = int(input("Введите c: "))
+d = int(input("Введите d: "))
+
+if a == d:
+    print(b * c)
+if b == d:
+    print(a* c)
+if c == d:
+    print(a * b)
