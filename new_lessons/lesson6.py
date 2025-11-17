c90d5aeb6778a96fe28734845505284416c59a9f
Funnyweek <kirito.sempai245@gmail.com>
2025-11-17 17:46:50 +0500

    123

--- new_lessons/lesson6.py
+++ new_lessons/lesson6.py
@@ -45,15 +45,37 @@
 # for name in range(len(names)):
 #    print(name, names[name])
 
-numbers = [2, 4, 6, 8, 10, 12, 15, 18, 20]
-list_5 = []
-for number in numbers:
-    print(number * 5)
+# numbers = [2, 4, 6, 8, 10, 12, 15, 18, 20]
+# list_5 = []
+# for number in numbers:
+#     print(number * 5)
+# print("----------------")
+# for number_2 in numbers:
+#     print(number_2 ** 2)
+# print("----------------")
+# for number_3 in numbers:
+#     if number_3 % 3 == 0:
+#         list_5.append(number_3)
+# print(list_5)
+
+list = ['python', 'javascript', 'go', 'rust', 'java', 'cpp']
+a = []
+v= []
+word = max(list, key=len)
+print(word)
+word_1 = min(list, key=len)
+print(word_1)
 print("----------------")
-for numbers_1 in numbers:
-    print(number ** 2)
-print("----------------")
-for number_3 in numbers:
-    if number_3[number_3] // 3:
-        list_5.append(number_3)
-print(list_5)
+for s in list:
+    s = list.index(s)
+    print(f"{list[s][0]}")
+    print(f"{list[s][-1]}")
+    print(f"{list[s]} {len(list[s])}")
+for d in list:
+    c = d.capitalize()
+    a.append(c)
+print(a)
+for m in list:
+    if "p" in m:
+        v.append(m)
+print(v)
