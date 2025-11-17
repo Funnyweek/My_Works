51715448ba95c03f97e236ed28385ca54566b86e
Funnyweek <kirito.sempai245@gmail.com>
2025-11-17 21:33:07 +0500

    17.11.25-2

--- new_lessons/calculator.py
+++ new_lessons/calculator.py
@@ -1,12 +1,19 @@
-a = int(input("a = "))
-c = input("Что вы хотите?(+ - * /) = ")
-b = int(input("b = "))
+while True:
+    a = int(input("a = "))
+    c = input("Что вы хотите?(+ - * /) = ")
+    b = int(input("b = "))
 
-if c == "+":
-    print (f"{a} + {b} = {a+b}")
-elif c == "-":
-    print (f"{a} - {b} = {a-b}")
-elif c == "*":
-    print (f"{a} * {b} = {a*b}")
-elif c == "/":
-    print (f"{a} / {b} = {a/b}")
+    if c == "+":
+        print (f"{a} + {b} = {a+b}")
+    elif c == "-":
+        print (f"{a} - {b} = {a-b}")
+    elif c == "*":
+        print (f"{a} * {b} = {a*b}")
+    elif c == "/":
+        print (f"{a} / {b} = {a/b}")
+    else:
+        print("Error")
+    exit = input("Exit? (yes/no) = ")   
+    if exit == "yes" or exit == "y" or exit == "exit" or exit == "EXIT" or exit == "Exit":
+        print("Goodbye")    
+        break
