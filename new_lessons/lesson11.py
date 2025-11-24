01025524103636ecad157f66ff16953cc2f825a0
Funnyweek <kirito.sempai245@gmail.com>
2025-11-24 19:35:05 +0500

    123

---
+++ new_lessons/lesson11.py
@@ -0,0 +1,21 @@
+def sum(a, c):
+    return a + c
+b = int(input('>>>'))
+d = int(input('>>>'))
+
+z = sum(b , d)
+print(z) 
+
+print(z +  35)
+
+l = input('>>>')
+x = 'sadsadasdasdsadsadasd'
+z = input(">>>>>>>>")
+def ount(text):
+    q = 0
+    for i in text:
+        q += 1
+    return q
+print(ount(l))
+print(ount(x))
+print(ount(z))
